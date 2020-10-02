1fe8f2760c76fb7c47834caa48e0e096f2541f75
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-10-02 14:16:53 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+// Created by: Magnus
+// 
+// Created on: Oct 2020
+// 
+// this program uses the sonar to dictate what color the neopixels will change to.
+let strip2: neopixel.Strip = null
 let distance_to_object = 0
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
@@ -6,7 +12,5 @@ basic.forever(function () {
     DigitalPin.P2,
     PingUnit.Centimeters
     )
-    if (distance_to_object == 0) {
-    	
-    }
+    strip2 = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 })
