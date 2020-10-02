5d2d1cea1a5258d5ee66f03305458bee31804a72
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-10-02 14:30:38 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,14 +3,12 @@
 // Created on: Oct 2020
 // 
 // this program uses the sonar to dictate what color the neopixels will change to.
-let strip2: neopixel.Strip = null
-let distance_to_object = 0
+let distance_from_sonar = 0
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    distance_to_object = sonar.ping(
+    distance_from_sonar = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
     )
-    strip2 = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 })
