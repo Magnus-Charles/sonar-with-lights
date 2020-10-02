e80b65d83af2eb4be2896daee361076fb91a5362
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-10-02 14:07:35 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
+let distance_to_object = 0
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-	
+    distance_to_object = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    if (distance_to_object == 0) {
+    	
+    }
 })
