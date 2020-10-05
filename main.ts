9ecaa133b0b666a238d78c46910d58e578f87c94
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-10-05 16:58:40 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,19 +12,26 @@ basic.forever(function () {
     DigitalPin.P2,
     PingUnit.Centimeters
     )
+    strip2.clear()
     if (distance_from_sonar > 4) {
         strip2 = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
         strip2.showColor(neopixel.colors(NeoPixelColors.Green))
+        basic.pause(100)
+        strip2.clear()
     }
     basic.pause(5000)
     if (distance_from_sonar <= 4) {
         strip2 = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
         strip2.showColor(neopixel.colors(NeoPixelColors.Blue))
+        basic.pause(100)
+        strip2.clear()
     }
     basic.pause(5000)
     if (distance_from_sonar <= 1) {
         strip2 = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
         strip2.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.pause(100)
+        strip2.clear()
     }
     basic.pause(5000)
 })
